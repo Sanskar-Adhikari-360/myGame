7be20e7bc7f01e9f0b4f74c3bc90e3c9310d961e
Sanskar-Adhikari-360 <adhikarisanskar360@gmail.com>
2024-10-23 10:33:58 +0545

    flappy bird

--- main.lua
+++ main.lua
@@ -1,20 +1,48 @@
 _G.love = require("love")
-
+wf =  require("libaries/windfield")
 function love.load()
+    screen = {
+        w = love.graphics.getWidth(),
+        h = love.graphics.getHeight()
+    }
+
+    world = wf.newWorld(0,500)
+    
     player = {}
-    player.x =  50
-    player.y = 150
-    player.sprite = love.graphics.newImage('sprites/bluebird-upflap.png')
+    
+    player.x = 100
+    player.y = 100
+    player.speed = 300
+    -- player.collider = world:newRectangleCollider(player.x,player.y,35,35)
+    player.sprite = love.graphics.newImage("sprites/bluebird-upflap.png")
+
+    background = love.graphics.newImage("sprites/background.png")
+    pipe = love.graphics.newImage("sprites/pipe.png")
+    ground  = love.graphics.newImage("sprites/ground.png")
+    box = world:newRectangleCollider(player.x,player.y,35,35)
 end
 function love.update(dt)
-    if love.keyboard.isDown('q') then
+    player.x =  box:getX()
+    player.y =  box:getY()
+
+
+    if love.keyboard.isDown("q") then
         love.event.quit()
     end
-    if love.keyboard.isDown('w') then
-        player.y = player.y - 5
-    end
-    player.x = player.x + 5
+
+    -- box:setLinearVelocity(vx,vy)
+    player.x = box:getX()
+    player.y = box:getY()
+    world:update(dt)
 end
 function love.draw()
-    love.graphics.draw(player.sprite,player.x,player.y)
+    love.graphics.draw(background,0,0)
+    love.graphics.draw(ground,0,(screen.h - 16))
+    love.graphics.draw(player.sprite,player.x,player.y,nil,nil,nil,15,12)
+    world:draw()
 end
+function love.keypressed(key)
+    if key == 'space' then
+      box:applyLinearImpulse(0, -1000)
+    end
+  end
