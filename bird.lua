657ed5d6c06329025a337178408cdb2e7413b157
Sanskar-Adhikari-360 <adhikarisanskar360@gmail.com>
2024-10-23 21:38:34 +0545

    added a new file

---
+++ bird.lua
@@ -0,0 +1,15 @@
+screen = {
+    w = love.graphics.getWidth(),
+    h = love.graphics.getHeight()
+}
+
+bird = {}
+bird.image  = love.graphics.newImage("sprites/bluebird-upflap.png")
+bird.width = bird.image:getWidth()
+bird.height = bird.image:getHeight()
+bird.x = screen.w/4
+bird.y = screen.h/2
+
+function birdRender()
+    love.graphics.draw(bird.image,bird.x,bird.y)
+end
--- main.lua
+++ main.lua
@@ -1,20 +1,21 @@
 _G.love = require("love")
 wf =  require("libaries/windfield")
 function love.load()
+    require 'bird'
     screen = {
         w = love.graphics.getWidth(),
         h = love.graphics.getHeight()
     }
 
-    world = wf.newWorld(0,500)
+    world = wf.newWorld(0,0)
     
-    player = {}
-    
-    player.x = 100
-    player.y = 100
-    player.speed = 300
+    -- player = {}
+    -- 
+    -- player.x = 100
+    -- player.y = 100
+    -- player.speed = 300
     -- player.collider = world:newRectangleCollider(player.x,player.y,35,35)
-    player.sprite = love.graphics.newImage("sprites/bluebird-upflap.png")
+    -- player.sprite = love.graphics.newImage("sprites/bluebird-upflap.png")
 
     background = love.graphics.newImage("sprites/background.png")
     backgroundScroll = 0
@@ -25,11 +26,11 @@ function love.load()
     BACKGROUND_SCROLL_SPEED = 30
     GROUND_SCROLL_SPEED = 60
     BACKGROUND_LOOPING_POINT = screen.w/6.5
-    box = world:newRectangleCollider(player.x,player.y,35,35)
+    -- box = world:newRectangleCollider(player.x,player.y,35,35)
 end
 function love.update(dt)
-    player.x =  box:getX()
-    player.y =  box:getY()
+    -- player.x =  box:getX()
+    -- player.y =  box:getY()
 
 
     if love.keyboard.isDown("q") then
@@ -37,8 +38,8 @@ function love.update(dt)
     end
 
     -- box:setLinearVelocity(vx,vy)
-    player.x = box:getX()
-    player.y = box:getY()
+    -- player.x = box:getX()
+    -- player.y = box:getY()
     world:update(dt)
 
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
@@ -49,11 +50,12 @@ end
 function love.draw()
     love.graphics.draw(background,-backgroundScroll,0)
     love.graphics.draw(ground,-groundScroll,(screen.h - 16))
-    love.graphics.draw(player.sprite,player.x,player.y,nil,nil,nil,15,12)
+    -- love.graphics.draw(player.sprite,player.x,player.y,nil,nil,nil,15,12)
+    birdRender()
     world:draw()
 end
-function love.keypressed(key)
-    if key == 'space' then
-      box:applyLinearImpulse(0, -1000)
-    end
-  end
+-- function love.keypressed(key)
+--     if key == 'space' then
+--       box:applyLinearImpulse(0, -1000)
+--     end
+--   end
